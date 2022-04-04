a48b1dc4aea412886cec4bad902670b64d4ee4e4
Rohan Pareek <rohaniscoding@gmail.com>
2022-04-04 15:25:22 +0530

    Initializing twitter bot

---
+++ app.js
@@ -0,0 +1,25 @@
+const Twitter = require('twitter');
+const keys = require('./config');
+
+const client = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret,
+});
+
+// create a stream
+const stream = client.stream('statuses/filter', { track: '#html #css #javascript' });
+
+stream.on('data', function (tweet) {
+
+    // like the found tweet
+    client.post('favorites/create', { id: tweet.id_str }).then(result => {
+        console.log('LIKED : "' + result.text + '"');
+    }).catch(console.error);
+
+    // retweet the found tweet
+    client.post('statuses/retweet', { id: tweet.id_str }).then(result => {
+        console.log('RETWEETED : "' + result.text + '"');
+    }).catch(console.error);
+});
---
+++ config.js
@@ -0,0 +1,8 @@
+const TWITTER_KEYS = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+}
+
+module.exports = TWITTER_KEYS;
